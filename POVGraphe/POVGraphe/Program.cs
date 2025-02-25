898ff830b380cd75d6d70706752420fe49c64ad0
lvciie <lucie.grandet@edu.devinci.fr>
2025-02-25 12:25:53 +0100

    Update Program.cs

--- POVGraphe/POVGraphe/Program.cs
+++ POVGraphe/POVGraphe/Program.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //lire le fichier soc-karate.mtx
+            //instencier dans le graphe (en utilisant les 2 modes)
+            //répondre aux questions (parcours, connexe, cycle ?)
+            //+ outils de visualisation d'un graphe (Microsoft System.Drawing)
+
+
             Graphe monGraphe = new Graphe(6);
             monGraphe.AjouterLien(0, 1);
             monGraphe.AjouterLien(0, 2);
+            
             monGraphe.AjouterLien(1, 3);
 
             monGraphe.ParcoursProfondeur(2);
