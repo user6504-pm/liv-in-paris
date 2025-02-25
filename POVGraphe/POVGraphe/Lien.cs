19ab9ccb9db79abfed91ca1b076adc70a5e68c62
lvciie <lucie.grandet@edu.devinci.fr>
2025-02-25 09:16:11 +0100

    Add files via upload

---
+++ POVGraphe/POVGraphe/Lien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Lien
+{
+    public Noeud Source;
+    public Noeud Cible;
+    public Lien(Noeud source, Noeud cible)
+    {
+        Source = source;
+        Cible = cible;
+    }
+}
+
---
+++ POVGraphe/POVGraphe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graphe monGraphe = new Graphe(6);
+            monGraphe.AjouterLien(0, 1);
+            monGraphe.AjouterLien(0, 2);
+            monGraphe.AjouterLien(1, 3);
+
+            monGraphe.ParcoursProfondeur(2);
+        }
+
+    }
+}
